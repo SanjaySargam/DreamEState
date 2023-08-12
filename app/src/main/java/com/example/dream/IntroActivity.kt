df19852ec11f533d201ada55194cc0f0f08095b5
Sanjaykumar Sargam <sargamsanjaykumar@gmail.com>
2023-08-12 16:17:52 +0530

    intent

--- app/src/main/java/com/example/dream/IntroActivity.kt
+++ app/src/main/java/com/example/dream/IntroActivity.kt
@@ -1,11 +1,20 @@
 package com.example.dream
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class IntroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
+        val startBtn = findViewById<Button>(R.id.start)
+
+        startBtn.setOnClickListener {
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
