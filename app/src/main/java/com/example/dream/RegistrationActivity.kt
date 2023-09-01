7913c435c60885b7688d482b60d3fe774ec7d9a8
Sanjay Sargam <sargamsanjaykumar@gmail.com>
2023-09-01 15:47:43 +0530

    setonclicklogin

--- app/src/main/java/com/example/dream/RegistrationActivity.kt
+++ app/src/main/java/com/example/dream/RegistrationActivity.kt
@@ -1,11 +1,20 @@
 package com.example.dream
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class RegistrationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
+        val login = findViewById<TextView>(R.id.login)
+        login.setOnClickListener{
+            val intent = Intent(this@RegistrationActivity,MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
