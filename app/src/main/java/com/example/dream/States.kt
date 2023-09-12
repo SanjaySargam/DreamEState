e7b4413af52f7c2cdd9cc16cd1c74ffcd2a4df62
Sanjay Sargam <sargamsanjaykumar@gmail.com>
2023-09-12 17:36:16 +0530

    States and District added

---
+++ app/src/main/java/com/example/dream/States.kt
@@ -0,0 +1,7 @@
+data class State(val name: String, val districts: List<String>)
+
+val states = listOf(
+    State("Andhra Pradesh", listOf("District A", "District B", "District C")),
+    State("Tamil Nadu", listOf("District X", "District Y", "District Z")),
+    // Add more states and districts here
+)
